d6d34a43babe083c3ac8a83b79c21cbf38278cac
Bigizic <olalekanisaac75@gmail.com>
2024-06-04 23:23:03 +0100

    add updates to js files and render.js

--- dashboard/js/dashboard.js
+++ dashboard/js/dashboard.js
@@ -2,11 +2,88 @@
  * dashboardCalendar - redners calender on dashboard
 */
 
+(function($){		
+	$.fn.createCalendar = function(options){
+
+    const settings = $.extend({
+      color: 'blue'
+    }, options);
+    
+    // Apply the color to each selected element
+    return this.each(function(){
+      `<div class="c-container">
+    <div class="calendar_background">
+    <div class="calendar-base">
+      <div class="year"></div>
+      <div class="months">
+        <span class="month-hover">Jan</span>
+        <span class="month-hover">Feb</span> 
+        <span class="month-hover">Mar</span> 
+        <span class="month-hover">Apr</span>
+        <span class="month-hover">May</span>
+        <span class="month-hover">Jun</span>
+        <span class="month-hover">Jul</span> 
+        <span class="month-hover">Aug</span> 
+        <span class="month-hover">Sep</span> 
+        <span class="month-hover">Oct</span> 
+        <span class="month-hover">Nov</span> 
+        <span class="month-hover">Dec</span>
+      </div>
+  
+      <div class="days">
+        <div class="mon">
+          <p>MON</p>
+          <ul style="list-style-type: none; color: black; font-weight: lighter;"></ul>
+        </div>
+        <div class="tue">
+          <p>TUE</p>
+          <ul style="list-style-type: none; color: black; font-weight: lighter;"></ul>
+        </div>
+        <div class="wed">
+          <p>WED</p>
+          <ul style="list-style-type: none; color: black; font-weight: lighter;"></ul>
+        </div>
+        <div class="thu">
+          <p>THU</p>
+          <ul style="list-style-type: none; color: black; font-weight: lighter;"></ul>
+        </div>
+        <div class="fri">
+          <p>FRI</p>
+          <ul style="list-style-type: none; color: black; font-weight: lighter;"></ul>
+        </div>
+        <div class="sat">
+          <p>SAT</p>
+          <ul style="list-style-type: none; color: black; font-weight: lighter;"></ul>
+        </div>
+        <div class="sun">
+          <p>SUN</p>
+          <ul style="list-style-type: none; color: black; font-weight: lighter;"></ul>
+        </div>
+      </div>
+  </div>
+  </div>
+    <div class="calendar-left">
+
+      <div style="padding: 30px 0px 0px 0px; text-align: center;" class="num-date-and-day">
+      <div class="num-date"></div>
+      <div class="day"></div>
+    </div>
+      <div class="current-events">Current Events
+        <br/>
+        <ul>
+          <li style="font-size: 10px; margin-left: 10%;">Day 09 Daily CSS Image</li>
+        </ul>
+      </div>
+    </div>
+  </div>`
+    });
+    }
+})(jQuery);
+
 const $ = window.$;
 
 $(document).ready(function () {
   'use strict';
-
   // dashboard calendar rendering
   function dashboardCalendar () {
     const currentDate = new Date();
@@ -77,3 +154,6 @@ $(document).ready(function () {
     });
   }
   dashboardCalendar();
+});
+
+
---
+++ dashboard/js/render.js
@@ -0,0 +1,8 @@
+/**
+ * render.js
+ * Make a call to jquery function here
+ * 
+ */
+$(document).ready(function() {
+    $('.dashboard_render').createCalendar({color: 'red'});
+})
